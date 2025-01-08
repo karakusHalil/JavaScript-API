956fe01d91e15ca85d151ff5cedb3eaf4fceae77
karakusHalil <karakus_halil@outlook.com>
2025-01-08 14:51:17 +0300

    Completed

--- StartBootstrapTemplate/js/index.js
+++ StartBootstrapTemplate/js/index.js
@@ -1,22 +1,118 @@
-/* <div class="col mb-5">
-<div class="card h-100">
-    <!-- Product image-->
-    <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-    <!-- Product details-->
-    <div class="card-body p-4">
-        <div class="text-center">
-            <!-- Product name-->
-            <h5 class="fw-bolder">Fancy Product</h5>
-            <!-- Product price-->
-            $40.00 - $80.00
-        </div>
-    </div>
-    <!-- Product actions-->
-    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
-    </div>
-</div>
-</div> */
+
+const url = "http://localhost:8080/api";
 
 const productListElement = document.querySelector("#product-list");
-console.log(productListElement);
+//console.log(productListElement);
+
+
+
+const getProductById = async () => {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+    const response = await fetch(url+`/products/${id}`);
+    const data = await response.json();
+    const detailBody = document.querySelector("#detail-body");
+    const randomNumber = Math.floor(Math.random()*1000);
+    detailBody.innerHTML =`
+                <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
+                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            Cart
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- Product section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="row gx-4 gx-lg-5 align-items-center">
+                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://picsum.photos/600/700?random=${randomNumber}" alt="..." /></div>
+                    <div class="col-md-6">
+                        <div class="small mb-1">SKU: BST-498</div>
+                        <h1 class="display-5 fw-bolder">${data.name}</h1>
+                        <div class="fs-5 mb-5">
+                            <span class="text-decoration-line-through">$${data.price}</span>
+                            <span>$${data.price-(data.price/5)}</span>
+                        </div>
+                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
+                        <div class="d-flex">
+                            <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
+                            <button class="btn btn-outline-dark flex-shrink-0" type="button">
+                                <i class="bi-cart-fill me-1"></i>
+                                Add to cart
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    `;
+}
+getProductById();
+
+
+const getProducts = async() => {
+    const response = await fetch(url+"/products");
+    const data = await response.json();
+    console.log(data);
+    data.forEach(product => {
+        const randomNumber = Math.floor(Math.random()*1000);
+        productListElement.innerHTML += `
+            <div class="col mb-5">
+                <div class="card h-100">
+                    <!-- Product image-->
+                    <img class="card-img-top" src="https://picsum.photos/450/300?random=${randomNumber}" alt="...">
+                    <!-- Product details-->
+                    <div class="card-body p-4">
+                        <div class="text-center">
+                            <!-- Product name-->
+                            <h5 class="fw-bolder">${product.name}</h5>
+                            <!-- Product price-->
+                            $ ${product.price.toFixed(2)}
+                        </div>
+                    </div>
+                    <!-- Product actions-->
+                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent" >
+                        <div class="text-center"><a id="product-${product.id}" class="" target="_blank" href="product-detail.html?id=${product.id}">View Details</a></div>
+                    </div>
+                </div>
+                </div>
+        `;
+        //  const linkElement = document.getElementById(`product-${product.id}`);
+        // // console.log(linkElement);
+        // const button = document.createElement("button");
+        // button.id = `btn-${product.id}`;
+        // button.classList = "btn btn-outline-dark mt-auto";
+        // button.innerText = "View Details";
+        // button.addEventListener("click",getProductById());
+        // linkElement.append(button);
+    });
+}
+
+getProducts();
+
+
+
+
+
