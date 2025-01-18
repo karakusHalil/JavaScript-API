0a750711630ed70c4a077326537d12818f8f0fa8
karakusHalil <karakus_halil@outlook.com>
2025-01-18 04:02:55 +0300

    Add products route to the router

--- JS-Final-App/backend/routers/index.js
+++ JS-Final-App/backend/routers/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const auth = require("./auth");
 const category = require("./categories");
+const products = require("./products");
 
 router.use("/auth",auth);
 router.use("/categories", category);
+router.use("/products",products);
 
 module.exports = router;
